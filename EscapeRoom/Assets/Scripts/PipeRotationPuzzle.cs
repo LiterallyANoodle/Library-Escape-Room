6bed4d112abca4c98f9941ee701d5a565d425036
Austin Hampton <amh107@latech.edu>
2024-04-04 16:52:21 -0500

    Fixed Fully working Pipe Puzzle
    It is fully working greybox now.

--- EscapeRoom/Assets/Scripts/PipeRotationPuzzle.cs
+++ EscapeRoom/Assets/Scripts/PipeRotationPuzzle.cs
@@ -6,10 +6,14 @@ using UnityEngine.UI;
 using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine.XR;
 using System;
+
+
 public class PipeRotationPuzzle : MonoBehaviour
 {
     [SerializeField] private float rotationSpeed = 10f; // Speed of rotation (adjust as needed)
     public GameObject[] objectsToRotate; // Array of objects to rotate
+
+    public int isComplete = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -29,12 +33,23 @@ public class PipeRotationPuzzle : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float ActivationRandomNumber = UnityEngine.Random.Range(0, 100);
 
-        if(ActivationRandomNumber == 5)
+
+        if ((isComplete == 0) && (objectsToRotate[6].transform.eulerAngles.z == 90) && (objectsToRotate[3].transform.eulerAngles.z == 90 | objectsToRotate[3].transform.eulerAngles.z == 270) && (objectsToRotate[0].transform.eulerAngles.z == 180) && (objectsToRotate[1].transform.eulerAngles.z == 0 | objectsToRotate[1].transform.eulerAngles.z == 180) && (objectsToRotate[2].transform.eulerAngles.z == 180) && (objectsToRotate[5].transform.eulerAngles.z == 90 | objectsToRotate[5].transform.eulerAngles.z == 270) && (objectsToRotate[8].transform.eulerAngles.z == 270))
         {
-            int randomPipe = UnityEngine.Random.Range(0, 9);
-            objectsToRotate[randomPipe].transform.Rotate(Vector3.forward * rotationSpeed); ; // Rotate on Y-axis
+            isComplete = 1;
+            print("Congrats");
+        }
+
+        if (isComplete == 0)
+        {
+            float ActivationRandomNumber = UnityEngine.Random.Range(0, 1000);
+
+            if (ActivationRandomNumber == 5)
+            {
+                int randomPipe = UnityEngine.Random.Range(0, 9);
+                objectsToRotate[randomPipe].transform.Rotate(Vector3.forward * rotationSpeed); // Rotate on Y-axis
+            }
         }
 
 

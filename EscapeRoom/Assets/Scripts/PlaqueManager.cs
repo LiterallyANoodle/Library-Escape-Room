33eaac24820e5721619d6a3f391d41674a3be6a3
Troy Chiasson <chiassonmtroy@gmail.com>
2024-04-04 05:46:05 -0500

    Added "plaque pieces" and ability for them to snap to "statue"
    Still need to implement puzzle manager

---
+++ EscapeRoom/Assets/Scripts/PlaqueManager.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.XR.Interaction.Toolkit;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class PlaquePiece : MonoBehaviour
+{
+    public GameObject anchor;
+    public bool justReleased;
+
+    public void SelectEntered(SelectEnterEventArgs e) => this.justReleased = false;
+    public void SelectExited(SelectEnterEventArgs e) => this.justReleased = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.justReleased = false ;
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    private void SnapToPlace()
+    {
+        transform.SetPositionAndRotation(anchor.transform.position, anchor.transform.rotation);
+
+            XRGrabInteractable interactable = GetComponent<XRGrabInteractable>();
+            interactable.enabled = false;
+
+            Rigidbody body = GetComponent<Rigidbody>();
+            body.useGravity = false;
+            body.isKinematic = true;
+    }
+
+    public void checkRangeToSnap()
+    {
+        // Can't figure out why check doesnt work correctly? I just put a bang to skip it for now
+        if (!justReleased)
+        {
+            float distanceToAnchor = Vector3.Distance(this.transform.position, anchor.transform.position);
+            if (distanceToAnchor < 0.1f)
+            {
+                SnapToPlace();
+            }
+        }
+    }
+}

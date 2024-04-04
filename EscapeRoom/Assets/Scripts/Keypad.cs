0a8558f9a07d4cc9dfc561031492186dc115d93e
BlueJayBird11 <51869746+BlueJayBird11@users.noreply.github.com>
2024-04-04 16:09:53 -0500

    Put keypad in presentation scene

---
+++ EscapeRoom/Assets/Scripts/Keypad.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Keypad : PuzzleManager
+{
+    public bool check = false;
+    List<int> inputted = new List<int>();
+    List<int> answer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        answer = new List<int>() {1,2,3};
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Testfun(int i) {
+        inputted.Add(i);
+        foreach (int j in inputted)
+        {
+            print(j);
+        }
+    }
+
+    public void ButtonClear() {
+        inputted = new List<int>();
+    }
+
+    public void ButtonSubmit() {
+        check = true;
+        if(inputted.Count != answer.Count){
+            print("false");
+            check = false;
+        }
+        if(inputted.Count == answer.Count){
+            for (int k = 0; k < inputted.Count; k++) {
+                if (answer[k] != inputted[k]) {
+                    print("false");
+                    check = false;
+                }
+            }
+        }
+    }
+    public override bool verifySolved() {
+        return check;
+    }
+}

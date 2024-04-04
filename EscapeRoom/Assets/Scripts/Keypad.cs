9ff5832ef861215f759d04a52bf7ab6035f4b398
s0nicx2729 <157264174+s0nicx2729@users.noreply.github.com>
2024-04-03 19:41:43 -0500

    Submission button.

--- EscapeRoom/Assets/Scripts/Keypad.cs
+++ EscapeRoom/Assets/Scripts/Keypad.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class Keypad : MonoBehaviour
 {
     List<int> inputted = new List<int>();
+    List<int> answer;
     // Start is called before the first frame update
     void Start()
     {
-        
+        answer = new List<int>() {1,2,3};
     }
 
     // Update is called once per frame
@@ -29,5 +30,19 @@ public class Keypad : MonoBehaviour
     }
 
     public void ButtonSubmit() {
+        bool check = true;
+
+        if(inputted.Count != answer.Count){
+            check = false;
+            print("false");
+        }
+        if(inputted.Count == answer.Count){
+            for (int k = 0; k < inputted.Count; k++) {
+                if (answer[k] != inputted[k]) {
+                    check = false;
+                    print("false");
+                }
+            }
+        }
     }
 }

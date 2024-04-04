484c6f5b749f04d205fa8dd09b64011d883d5503
Austin Hampton <amh107@latech.edu>
2024-04-04 15:05:58 -0500

    Added Baseline for PipePuzzle
    Added Pipe Puzzle.

---
+++ EscapeRoom/Assets/Scripts/PipeRotationPuzzle.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR;
+using System;
+public class PipeRotationPuzzle : MonoBehaviour
+{
+    [SerializeField] private float rotationSpeed = 10f; // Speed of rotation (adjust as needed)
+    public GameObject[] objectsToRotate; // Array of objects to rotate
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("hello");
+    }
+
+
+    public void rotatePipe(int i)
+    {
+
+        objectsToRotate[i].transform.Rotate(Vector3.forward * rotationSpeed); ; // Rotate on Y-axis
+    
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        float ActivationRandomNumber = UnityEngine.Random.Range(0, 50);
+
+        if(ActivationRandomNumber == 5)
+        {
+            int randomPipe = UnityEngine.Random.Range(0, 9);
+            objectsToRotate[randomPipe].transform.Rotate(Vector3.forward * rotationSpeed); ; // Rotate on Y-axis
+        }
+
+
+    }
+
+}

128169109c8e8bd94ad7a14b3823f44cde1eb8a8
BlueJayBird11 <51869746+BlueJayBird11@users.noreply.github.com>
2024-04-10 23:46:42 -0500

    Fixed floating book state issue

--- EscapeRoom/Assets/Scripts/FloatingBook.cs
+++ EscapeRoom/Assets/Scripts/FloatingBook.cs
@@ -2,23 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum FloatingBookState
+{
+    GRABBED,
+    IDLE
+}
+
 public class FloatingBook : MonoBehaviour
 {
     private Rigidbody rb;
     public float speed = 5000;
+    private FloatingBookState state;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        state = FloatingBookState.IDLE;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float multiplier = (Mathf.Sin(Time.time) + speed) / speed;
-        // print(multiplier);
-        transform.position = new Vector3(transform.position.x, transform.position.y * multiplier, transform.position.z);
-        ResetVelocity();
+        if (state == FloatingBookState.IDLE)
+        {
+            float multiplier = (Mathf.Sin(Time.time) + speed) / speed;
+            // print(multiplier);
+            transform.position = new Vector3(transform.position.x, transform.position.y * multiplier, transform.position.z);
+            ResetVelocity();
+        }
     }
 
     public void ResetVelocity()
@@ -26,4 +37,16 @@ public class FloatingBook : MonoBehaviour
         rb.velocity = Vector3.zero;
         rb.angularVelocity = Vector3.zero;
     }
+
+    public void ChangeState(int newState)
+    {
+        if (newState == 0)
+        {
+            state = FloatingBookState.IDLE;
+        }
+        else if (newState == 1)
+        {
+            state = FloatingBookState.GRABBED;
+        }
+    }    
 }

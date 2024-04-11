f7d0d81fde212533441b32d4c7ce72590fd563f5
BlueJayBird11 <51869746+BlueJayBird11@users.noreply.github.com>
2024-04-11 15:35:04 -0500

    Added conduit puzzle

--- EscapeRoom/Assets/Scripts/Portal.cs
+++ EscapeRoom/Assets/Scripts/Portal.cs
@@ -6,16 +6,6 @@ public class Portal : MonoBehaviour
 {
     public Game game;
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     private void OnTriggerEnter(Collider other)
     {
